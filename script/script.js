1aa29f20314298a9b4aa34cc6660f43d009a73b0
sudonut <paradoxotl@gmail.com>
2021-04-30 15:01:15 -0400

    added weather pull up bar and some js functionality

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.getElementById('slide-btn').addEventListener('click', () => {
+  const displayer = document.querySelector('.main-content');
+  displayer.classList.toggle('active-anim');
+});
