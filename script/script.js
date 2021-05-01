bd89d53ec4398831d6229f084eb4ffc9515ccbd9
sudonut <paradoxotl@gmail.com>
2021-05-01 17:30:38 -0400

    small fixes

--- script/script.js
+++ script/script.js
@@ -11,21 +11,18 @@ document.getElementById('slide-btn').addEventListener('click', () => {
 
 
 
-function timeParse() {
+function getTime() {
   let time = new Date();
-  let hours = time.getHours() > 12 ? time.getHours() - 1 : time.getHours();
+  let hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
   let minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-  let seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-  let am_pm = time.getHours() >= 12 ? "PM" : "AM"
-
+  // let seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  // let am_pm = time.getHours() >= 12 ? "PM" : "AM"
+  
   if (hours === 0) {
     hours = 12;
   }
-  
+
   document.getElementById('time').innerHTML = `${hours}:${minutes}`;
-
-  // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
-
 }
 
 function backgroundGreet() {
@@ -36,14 +33,18 @@ function backgroundGreet() {
   if (hour >= 5 && hour < 12) {
     // Morning
     greeting.textContent = "GOOD MORNING, IT'S CURRENTLY"
+    document.body.style.background = "url(./imgs/morning.jpg) no-repeat center / cover"
   } else if (hour >= 12 && hour < 18) {
     // Afternoon
     greeting.textContent = "GOOD AFTERNOON, IT'S CURRENTLY"
+    document.body.style.background = "url(./imgs/day.jpg) no-repeat center / cover"
   } else {
     // Evening
     greeting.textContent = "GOOD EVENING, IT'S CURRENTLY"
+    document.body.style.background = "url(./imgs/night.jpg) no-repeat center / cover"
   }
+  document.getElementById('week-day').innerHTML = today.getDay() === 0 ? today.getDay() = 1 : today.getDay();
 }
 
 backgroundGreet();
-setInterval(timeParse, 1000);
+setInterval(getTime, 1000);
--- script/utils.js
+++ script/utils.js
@@ -1,20 +1,25 @@
-const apiKey = 'f06901ddfa0b553c54b46766f018b3b4'
-
+const apiKey = "f06901ddfa0b553c54b46766f018b3b4";
 
 function getPos(pos) {
   let cords = pos.coords;
-  const lat = cords.latitude
-  const lon = cords.longitude
+  const lat = cords.latitude;
+  const lon = cords.longitude;
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    let city = data.name.toUpperCase();
-    let country = data.sys.country;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let city = data.name.toUpperCase();
+      let country = data.sys.country;
 
-    document.getElementById('location').innerHTML = `IN ${city}, ${country}`;
-    document.getElementById('current-tz').innerHTML = (Intl.DateTimeFormat().resolvedOptions().timeZone).replace("_", ' ');
-  });
+      document.getElementById("location").innerHTML = `IN ${city}, ${country}`;
+      document.getElementById(
+        "current-tz"
+      ).innerHTML = Intl.DateTimeFormat()
+        .resolvedOptions()
+        .timeZone.replace("_", " ");
+    });
 }
 
 navigator.geolocation.getCurrentPosition(getPos);
