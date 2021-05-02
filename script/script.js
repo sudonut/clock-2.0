d9e334858a16c9687a1b549b05f899624cae8e02
sudonut <paradoxotl@gmail.com>
2021-05-02 14:54:39 -0400

    added display for current day of th week and current day of the year

--- script/script.js
+++ script/script.js
@@ -10,13 +10,13 @@ document.getElementById('slide-btn').addEventListener('click', () => {
 });
 
 
-
+// Fetch current time to display
 function getTime() {
   let time = new Date();
   let hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
   let minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-  // let seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-  // let am_pm = time.getHours() >= 12 ? "PM" : "AM"
+  let seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  let am_pm = time.getHours() >= 12 ? "PM" : "AM"
   
   if (hours === 0) {
     hours = 12;
@@ -25,6 +25,7 @@ function getTime() {
   document.getElementById('time').innerHTML = `${hours}:${minutes}`;
 }
 
+// Background change on time of day
 function backgroundGreet() {
   let greeting = document.getElementById('greet');
   let today = new Date();
@@ -43,8 +44,17 @@ function backgroundGreet() {
     greeting.textContent = "GOOD EVENING, IT'S CURRENTLY"
     document.body.style.background = "url(./imgs/night.jpg) no-repeat center / cover"
   }
-  document.getElementById('week-day').innerHTML = today.getDay() === 0 ? today.getDay() = 1 : today.getDay();
 }
 
-backgroundGreet();
+function dayInfo() {
+  let day = new Date();
+
+  document.getElementById('week-day').innerHTML = day.getDay() + 1;
+
+  document.getElementById('day-of-year').innerHTML = (Math.ceil((day - new Date(day.getFullYear(), 0, 1)) / 86400000));
+}
+
 setInterval(getTime, 1000);
+backgroundGreet();
+dayInfo();
+
